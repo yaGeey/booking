cfd958731762ce5b3107bbe58e981a1314bfcabf
yaGeey <krabadaj@gmail.com>
2025-06-17 03:12:17 +0300

    another docker fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -20,5 +20,8 @@ export default defineConfig({
    },
    server: {
       host: true,
+   },
+   build: {
+      target: 'esnext',
    }
 })
--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,5 @@
 import cookieParser from 'cookie-parser'
 import cors from 'cors'
-import 'dotenv/config'
 import express from 'express'
 import fs from 'fs'
 import helmet from 'helmet'
